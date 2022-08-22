d09f0c42a7cb19557f9d0a5a5b95a508922e3807
Jose Linardo <joligo94861@gmail.com>
2022-08-21 21:00:05 -0500

    Create-React-config-enviroment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+ 
+
+  return (
+    <div >
+      <header className='app-header'>
+          <a href='/'>Mi Tienda</a>
+      </header>
+      <main>
+        list Products
+      </main>
+    </div>
+  )
+}
+
+export default App
